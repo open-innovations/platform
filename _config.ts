6cbccd03289b6e2661122521b48d6779e0e309db
Giles Dring <giles@dringtech.com>
2023-03-03 15:33:35 +0000

    Create initial site

---
+++ _config.ts
@@ -0,0 +1,25 @@
+import lume from "lume/mod.ts";
+import base_path from "lume/plugins/base_path.ts";
+import date from "lume/plugins/date.ts";
+import inline from 'lume/plugins/inline.ts';
+import metas from "lume/plugins/metas.ts";
+import postcss from "lume/plugins/postcss.ts";
+
+const site = lume({
+  src: './docs',
+  location: new URL("https://open-innovations.github.io/platform/"),
+});
+
+site.use(base_path());
+site.use(inline());
+site.use(metas());
+site.use(date());
+site.use(postcss({}));
+
+site.copy('CNAME');
+site.copy('.nojekyll');
+
+site.remoteFile('assets/images/icons/cc.svg', 'https://mirrors.creativecommons.org/presskit/icons/cc.svg')
+site.remoteFile('assets/images/icons/by.svg', 'https://mirrors.creativecommons.org/presskit/icons/by.svg')
+
+export default site;
---
+++ docs/homepage/index.md
@@ -0,0 +1,14 @@
+---
+title: Welcome
+url: /
+templateEngine: njk,md
+---
+
+This site documents the Open Innovations plaform: the way that we build our projects.
+It is mainly designed to act as a reference for the team, but we've made it available
+under a Creative Commons Attribution {{ comp.license.part.cc_by() | safe }} license to
+allow anyone to freely reuse and remix it.
+
+For now, the site doesn't contain much more than a set of <a href='/principles/'>principles</a>,
+buy we've got a big plans to document the tools, techniques and libraries that we've defined and
+created over the last couple of years.
